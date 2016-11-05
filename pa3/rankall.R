3000cfb66e87da4bb21e238983db7388d80338ee
Ray Lugo, Jr <rlugojr@gmail.com>
2016-11-04 21:56:23 -0400

    Updates to PA3
    Updated rankall.R function.  Almost completed.  Needs tweaking to get
    exact use cases.

--- pa3/rankall.R
+++ pa3/rankall.R
@@ -46,39 +46,40 @@ rankall <- function(outcomeName = NULL, num = 1L){
   #remove rows with incomplete cases
   hospital_outcome <- na.omit(hospital_outcome)
 
-  #add column for ranking hospitals by score
-  hospital_outcome$rank <- NA
 
   #Sort by State, Score, HospitalName
   hospital_outcome <- hospital_outcome[order(hospital_outcome$State, hospital_outcome$Score, hospital_outcome$HospitalName),]
 
-  #hospital_outcome$rank <- unlist(with(hospital_outcome, tapply(Score,HospitalName,State,rank)))
-  #hospital_outcome$rank <- 1:nrow(hospital_outcome)
-  transform(hospital_outcome, rank <- ave(hospital_outcome$Score, hospital_outcome$State, FUN = function(x) rank(x, ties.method = "first")))
-  return(head(hospital_outcome))
-
+  #Assign ranking to each State grouping
+  hospital_outcome$ID <- 1:nrow(hospital_outcome)
+  hospital_outcome <- hospital_outcome[order(hospital_outcome$ID),]
+  hospital_outcome$Rank <- unlist(with(hospital_outcome, tapply(Score, State, function(x) rank(x,ties.method = "first"))))
+  #hospital_outcome
+  
   #set rank values for best or worst if passed as an argument
-  #maxrows = nrow(hospital_outcome)   #get total number of rows
-  #if (tolower(num) == "best") {
-    #best is 1 on our scale
-  #  rank = 1L
-  #}
-  #else if (tolower(num) == "worst") {
-    #worst is last row.
-  #  rank = as.integer(maxrows)
-  #}
-  #else if (num > maxrows) {
-    #if parameter value is greater than total rows then return NA
-  #  return(NA)
-  #}
-  #else {
-    #if non of the above apply, set rank = number given.
-  #  rank = as.integer(num)
-  #}
+  maxrows = nrow(hospital_outcome)   #get total number of rows
+  if (tolower(num) == "best") {
+      #best is 1 on our scale
+      rank <- 1L
+  }
+  else if (tolower(num) == "worst") {
+      #worst is last row.
+      rank <- as.integer(maxrows)
+  }
+  else if (num > maxrows) {
+      #if parameter value is greater than total rows then return NA
+      return(NA)
+  }
+  else {
+      #if non of the above apply, set rank = number given.
+      rank <- as.integer(num)
+  }
   #rank
-
-  #retrieve and display the ranked result
-  #rankResult <- hospital_outcome[rank,]
-  #rankResult
-
+  
+  #Return rows where the rank equals the num parameter
+  rankedResults <- subset(hospital_outcome, hospital_outcome$Rank == rank , select = c(HospitalName, State))
+  
+  rankedResults
+  
 }
+  
